cec2f99ac6c425253cbfde1ab83b05ac853df270
Devin Smith <devinsmith@deephaven.io>
2023-01-30 11:16:34 -0800

    Fix turbodbc wheels for pyarrow 11 (#40)

--- contexts/turbodbc-wheel/auditwheel-keep-pyarrow.py
+++ contexts/turbodbc-wheel/auditwheel-keep-pyarrow.py
@@ -4,8 +4,8 @@ from auditwheel.main import main
 from auditwheel.policy import _POLICIES
 
 for policy in _POLICIES:
-  policy['lib_whitelist'].append('libarrow_python.so.1000')
-  policy['lib_whitelist'].append('libarrow.so.1000')
+  policy['lib_whitelist'].append('libarrow_python.so')
+  policy['lib_whitelist'].append('libarrow.so.1100')
 
 if __name__ == '__main__':
   sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
