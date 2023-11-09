ece6d74fc62ff2452a9b499e4ac6da857840e999
Devin Smith <devinsmith@deephaven.io>
2023-11-09 07:23:33 -0800

    Bump turbodbc to 4.7.0 (#70)

--- contexts/turbodbc-wheel/auditwheel-keep-pyarrow.py
+++ contexts/turbodbc-wheel/auditwheel-keep-pyarrow.py
@@ -5,7 +5,7 @@ from auditwheel.policy import _POLICIES
 
 for policy in _POLICIES:
   policy['lib_whitelist'].append('libarrow_python.so')
-  policy['lib_whitelist'].append('libarrow.so.1200')
+  policy['lib_whitelist'].append('libarrow.so.1300')
 
 if __name__ == '__main__':
   sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
